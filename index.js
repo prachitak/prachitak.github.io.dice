bef85fd55d15342cc38141b2fb35b741e9556a5f
Prachita Khambalkar <prachita2016@gmail.com>
2020-04-19 14:36:05 +0530

    Dice game

---
+++ index.js
@@ -0,0 +1,20 @@
+function generateResult()
+{
+  var randomNumber1 = Math.ceil(Math.random() * 6);
+  document.querySelector(".img1").setAttribute("src","images/dice"+randomNumber1+".png");
+  var randomNumber2 = Math.ceil(Math.random() * 6);
+  document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png");
+
+  var result;
+
+  if(randomNumber1 === randomNumber2)
+    result = "Draw!";
+  else if (randomNumber1 > randomNumber2)
+    result = "🚩Pratik wins!";
+  else
+    result = "🚩Prachi wins!";
+
+  document.querySelector("h1").innerHTML = result;
+}
+
+window.onload = generateResult();
